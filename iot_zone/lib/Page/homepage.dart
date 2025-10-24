e328a3ae093fa52cb725e8d929131c6dbb79938f
Doid888 <6631501074@lamduan.mfu.ac.th>
2025-10-24 16:28:58 +0700

    flex hompage design first

--- iot_zone/lib/Page/homepage.dart
+++ iot_zone/lib/Page/homepage.dart
@@ -10,6 +10,44 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // ส่วนบน 30%
+            Expanded(
+              flex: 20, // 30%
+              child: Container(
+                color: Colors.deepPurple[200],
+                child: Center(
+                  child: Text(
+                    'ส่วนบน (20%)',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // ส่วนล่าง 70%
+            Expanded(
+              flex: 80, // 70%
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    'ส่วนล่าง (80%)',
+                    style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
