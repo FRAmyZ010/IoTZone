faf521a29e0ea44c4ef6d395c3ad02ab5c9ccf47
Pongsapat_P. <pongsapat.pp2004@gmail.com>
2025-11-04 22:15:50 +0700

    Add request status api

--- server/server.js
+++ server/server.js
@@ -213,6 +213,8 @@ app.patch('/assets/:id/status', (req, res) => {
   });
 });
 
+// ================== History API
+
 app.get('/api/history/:studentId', (req, res) => {
   const studentId = req.params.studentId;
   console.log('📩 API called: /api/history/' + studentId);
@@ -239,6 +241,41 @@ app.get('/api/history/:studentId', (req, res) => {
   db.query(sql, [studentId], (err, results) => {
     if (err) {
       console.error('❌ Error fetching history:', err);
+      res.status(500).json({ error: 'Database query failed', details: err });
+    } else {
+      console.log('✅ Query success, rows:', results.length);
+      res.json(results);
+    }
+  });
+});
+
+// ================= Request Status API
+
+app.get('/api/request-status/:studentId', (req, res) => {
+  const studentId = req.params.studentId;
+  console.log('📩 API called: /api/request-status/' + studentId);
+
+  const sql = `
+    SELECT 
+      a.asset_name AS name,
+      CASE 
+        WHEN h.status = 1 THEN 'Pending'
+        WHEN h.status = 2 THEN 'Borrowed'
+        ELSE 'Other'
+      END AS status,
+      h.borrow_date AS borrowDate,
+      h.return_date AS returnDate,
+      a.img AS image
+    FROM history h
+    JOIN asset a ON h.asset_id = a.id
+    WHERE h.borrower_id = ?
+      AND (h.status = 1 OR h.status = 2)
+    ORDER BY h.borrow_date DESC;
+  `;
+
+  db.query(sql, [studentId], (err, results) => {
+    if (err) {
+      console.error('❌ Error fetching request status:', err);
       res.status(500).json({ error: 'Database query failed', details: err });
     } else {
       console.log('✅ Query success, rows:', results.length);
