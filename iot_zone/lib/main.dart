6010ae80718ab31bcdc684ed01a75d1fb1acc3da
Doid888 <6631501074@lamduan.mfu.ac.th>
2025-11-14 03:15:31 +0700

    TestStudent03

--- iot_zone/lib/main.dart
+++ iot_zone/lib/main.dart
@@ -4,12 +4,27 @@ import 'package:iot_zone/Page/Asset_page/assetstaff.dart';
 import 'package:iot_zone/Page/Dashboard/borrow_requests_page.dart';
 import 'package:iot_zone/Page/Login/login_page.dart';
 import 'Page/Login/check_session_page.dart';
+import 'package:iot_zone/Page/homepagestaff.dart';
+import 'package:iot_zone/Page/homepagelender.dart';
+import 'Page/Widgets/buildBotttom_nav_bar/bottom_nav_bar.dart';
+import 'Page/Widgets/buildBotttom_nav_bar/bottom_nav_bar_staff.dart';
+import 'Page/Widgets/buildBotttom_nav_bar/bottom_nav_bar_lender.dart';
+
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CheckSessionPage(),
+      initialRoute: "/check",
+      routes: {
+        "/check": (context) => const CheckSessionPage(),
+        "/login": (context) => const LoginPage(),
+
+        // เพิ่มหน้า Main ของแต่ละ role เผื่อใช้ navigator
+        "/studentMain": (context) => const StudentMain(),
+        "/staffMain": (context) => const StaffMain(),
+        "/lenderMain": (context) => const LenderMain(),
+      },
     ),
   );
 }
